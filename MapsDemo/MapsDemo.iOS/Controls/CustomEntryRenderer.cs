f8ff63696c819f548623d8166d83b8af3402856c
Carlos Diaz <ryankar90@hotmail.com>
2020-08-03 17:48:27 -0500

    add

---
+++ MapsDemo/MapsDemo.iOS/Controls/CustomEntryRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+using System;
+using Xamarin.Forms.Platform.iOS;
+using MapsDemo.iOS.Controls;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace MapsDemo.iOS.Controls
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                Control.Layer.CornerRadius = 0;
+                Control.BorderStyle = UIKit.UITextBorderStyle.None;
+                Control.Layer.BorderWidth = 0;
+                Control.ClipsToBounds = false;
+            }
+        }
+    }
+}
