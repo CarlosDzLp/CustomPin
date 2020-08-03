4a39984736be95aa459aefdc93c509fe871b2e3d
Carlos Diaz <ryankar90@hotmail.com>
2020-08-03 16:36:02 -0500

    add

---
+++ MapsDemo/MapsDemo/Helpers/FontIcons.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MapsDemo.Helpers
+{
+    public class FontIcons
+    {
+        public const string Location = "\uf14e";
+        public const string Pin = "\uf276";
+        public const string Triangule = "\uf0d7";
+        public const string Arrow = "\uf060";
+    }
+}
